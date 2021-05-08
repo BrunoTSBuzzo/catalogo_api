ddf39b67cd79becd8bc5629d6fcd523570fdbf05
BrunoTSBuzzo <brunobuzzo.dev@gmail.com>
2021-05-08 14:33:32 -0300

    set secret key

--- config/settings/local.py
+++ config/settings/local.py
@@ -3,10 +3,7 @@ from .base import env
 
 DEBUG = True
 
-SECRET_KEY = env(
-    "DJANGO_SECRET_KEY",
-    default="7cqmA8mBZBE9sCaFPljRFVtMLpDJhFEb1cMZEfuFBx1QbXuJYrhJGtmfcjBVSfMn",
-)
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
